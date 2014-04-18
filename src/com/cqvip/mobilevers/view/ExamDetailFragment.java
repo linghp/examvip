a09383bbb5a0f6069236d0b9178abed9c7797573
soldierguard <luojiang515@gmail.com>
2014-04-18 16:50:28 +0800

    状态

--- src/com/cqvip/mobilevers/view/ExamDetailFragment.java
+++ src/com/cqvip/mobilevers/view/ExamDetailFragment.java
@@ -476,7 +476,11 @@ Log.i(TAG, "cancelled");
 							bundle.putString("id", subjectid);
 							intent.putExtra("bundle", bundle);
 							intent.putExtra("final", finalposition);
-							intent.putExtra("status", status);
+							if(isConinue){
+								intent.putExtra("status", status);
+							}else{
+								intent.putExtra("status", 0);
+							}
 							 startActivityForResult(intent, GET_CODE);
 						}
 					} else {
